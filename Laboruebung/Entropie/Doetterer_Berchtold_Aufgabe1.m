7877ae643d1f0f0cd76bf0451f149a17c4975aa9
HalfUnitato <marin.doetterer@gmail.com>
2021-03-29 16:53:34 +0200

    aufgabe1: done

--- Laboruebung/Entropie/Doetterer_Berchtold_Aufgabe1.m
+++ Laboruebung/Entropie/Doetterer_Berchtold_Aufgabe1.m
@@ -24,25 +24,24 @@ text = fileread('rfc2795.txt');
 [v, i] = maxk(y, 10);  %14 = h
 
 summe = sum(y);
-y_summe = y / summe;
-y_tmp = log2(1./y_summe);
+y_prob = y / summe;
+y_tmp = log2(1./y_prob);
 
-%{
 % Lineplot
-figure;
+figure(1);
 plot(x, y_tmp,'Color',[0,1.0,0]);
 xlabel('Zeichen (sortiert)');
 ylabel('Häufigkeit');
-%}
 
-%{
+
 % Barplot
+figure(2);
 b = bar(y_tmp);
 set(b,'FaceColor',[1.0,0.7,0]);
 set(gca,'xticklabel',char(x));
 xlabel('Zeichen');
 ylabel('Informationsgehalt');
-%}
+
 
 %{
 fprintf("Auftrittswahrscheinlichkeiten:")
@@ -53,11 +52,16 @@ end
 
 %% nr 4 - benoetigt nr3-code
 text = fileread('rfc2795.txt');
-[y, x] = groupcounts(double(text)') ;
-[v, i] = maxk(y, 10);  %14 = h
+[y, x] = groupcounts(double(text)');
 
 summe = sum(y);
-y_summe = y / summe;
-y_tmp = log2(1./y_summe);
+y_prob = y / summe;
+y_tmp = log2(1./y_prob);
 
-summe = sum(y_summe * y_tmp);
+entropy = sum(y_prob .* y_tmp);
+
+% sum prob / len(ASCII-char)
+% gleicheviele Bits fuer jedes Char (128 -> 7)
+redundancy = sum(y_prob .* log2(128));
+fprintf("Entropie der Nachrichtenquelle: %s\n", num2str(entropy));
+fprintf("Redundanz der Nachrichtenquelle: %s\n", num2str( redundancy - entropy));
